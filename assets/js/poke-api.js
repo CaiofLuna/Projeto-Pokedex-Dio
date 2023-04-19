9b1ba96355f7fa9fa0c8a79340c7384a50318138
CaiofLuna <c.luna.employment@gmail.com>
2023-04-19 16:05:24 -0300

    Project done

---
+++ assets/js/poke-api.js
@@ -0,0 +1,36 @@
+let pokeApi = {};
+
+function convertPokeApi_model(pokeDetail) {
+  let pokemon = new Pokemon();
+  pokemon.name = pokeDetail.name;
+  pokemon.number = pokeDetail.id;
+
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type;
+  pokemon.image = pokeDetail.sprites.front_default;
+  pokemon.stats = pokeDetail.stats.map((base_stats)=>base_stats.base_stat)
+
+  return pokemon;
+}
+
+pokeApi.getDetails = (pokemon) => {
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApi_model);
+};
+
+pokeApi.getPokemons = (offset = 0, limit = 24) => {
+  let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => responseJson.results)
+    .then((pokemons) => pokemons.map(pokeApi.getDetails))
+    .then((detailsResquest) => Promise.all(detailsResquest))
+    .then((pokeDetails) => pokeDetails)
+    .catch((error) => console.log(error))
+    .finally(() => console.log("Requisição concluída."));
+};
+
---
+++ assets/js/pokemodel.js
@@ -0,0 +1,12 @@
+class Pokemon {
+  name;
+  number;
+  types = [];
+  type;
+  image;
+  height;
+  weight;
+  abilities= [];
+  stats = [];
+  photo;
+}
